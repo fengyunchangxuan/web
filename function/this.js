ebcdaaeb33f4d395e686568c4e7edd430f693a86
cool <wangxy0436@foxmail.com>
2020-04-09 16:01:36 +0800

    funtion

---
+++ function/this.js
@@ -0,0 +1,34 @@
+/* 例 1. */
+var Obj = {
+  n: 1,
+  f: function () {
+    return this.n // f 函数 在 Obj 的作用域下执行，所以 this 代表了 Obj，this.n = Obj.n
+  }
+}
+
+n = Obj.f()
+console.log(n)
+
+
+/* 例 2. */
+var Obj = {
+  n: 1,
+  f: function () {
+    return (
+      () => {
+        return this.n
+      })()
+  } // 嵌套多层函数会导致 this 失效
+}
+
+n = Obj.f()
+console.log(n) // undefined
+
+
+/* 例 3. */
+var Obj = {
+  n: 1,
+  f: function () { return (() => this.n)() } // 箭头函数修复了嵌套多层函 this 失效的问题
+}
+n = Obj.f()
+console.log(n) // 1
